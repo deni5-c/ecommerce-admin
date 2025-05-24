026011f353b0ee7d012cc5746f666d14d1646a22
deni5-c <denisbobeica18@gmail.com>
2025-05-24 14:53:36 +0300

    Added: - useStoreModal - used to call interface of ModalStore -ModalProvide- let Client Model to be used on server side component as layout -StoreModal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,20 @@
 "use client"
-import Modal from "@/components/ui/modal"
-import Image from "next/image";
+import { useEffect } from "react";
 
+import { useStoreModal } from "@/hooks/use-store-modal";
 const SetupPage = () => {
+  const onOpen = useStoreModal((state) => state.onOpen)
+  const isOpen = useStoreModal((state) => state.isOpen)
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen])
+
   return (
     <div className="p-4">
-      <Modal title="Test" description="Test Desc" isOpen onClose={() => {}}>
-        Children
-      </Modal>
+      Root Page
     </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import {
-  ClerkProvider
-} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
+
+import { ModalProvider } from "@/providers/modal-provider";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -29,9 +30,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <ModalProvider />
+          
           {children}
         </body>
       </html>
