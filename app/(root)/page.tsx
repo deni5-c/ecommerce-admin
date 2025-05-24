5e6fbf084de2ef8bb27653f71f505e00b9abd91c
deni5-c <denisbobeica18@gmail.com>
2025-05-24 13:53:42 +0300

    Styling - centered sign-in & sign-out using layout - added profile-button

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,12 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
-export default function Home() {
+const SetupPage = () => {
   return (
     <div className="p-4">
-      <Button size="default" variant="default">Click</Button>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
+
+export default SetupPage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import {
-  ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
+  ClerkProvider
 } from "@clerk/nextjs";
 import "./globals.css";
 
@@ -37,15 +32,6 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <header className="flex justify-end items-center p-4 gap-4 h-16">
-            <SignedOut>
-              <SignInButton />
-              <SignUpButton />
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </header>
           {children}
         </body>
       </html>
