fca12dd9e179635bd1c391484fe326fcbcbdad5b
deni5-c <denisbobeica18@gmail.com>
2025-05-26 02:59:41 +0300

    Added API Alert

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -17,6 +17,7 @@ import { Separator } from "@/components/ui/separator"
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { AlertModal } from "@/components/modals/alert-modal"
+import { ApiAlert } from "@/components/ui/api-alert"
 
 interface SettingsFormProps {
     initialData: Store
@@ -115,6 +116,12 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
                     </Button>
                 </form>
             </Form>
+            <Separator />
+            <ApiAlert 
+                title="NEXT_PUBLIC_API_URL" 
+                description="Test desc"
+                variant="public"
+            />
         </>
     )
 }
