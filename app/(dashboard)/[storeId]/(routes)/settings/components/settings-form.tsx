8e443dd3814853aed058b7cd60acc29c5afe198c
deni5-c <denisbobeica18@gmail.com>
2025-05-26 03:11:41 +0300

    added useOrigin - added to get NEXT_PUBLIC_API_URL origin - to fix Hydration error

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -18,6 +18,7 @@ import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "
 import { Input } from "@/components/ui/input"
 import { AlertModal } from "@/components/modals/alert-modal"
 import { ApiAlert } from "@/components/ui/api-alert"
+import { useOrigin } from "@/hooks/use-origin"
 
 interface SettingsFormProps {
     initialData: Store
@@ -34,6 +35,8 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
 }) => {
     const params = useParams()
     const router = useRouter()
+    const origin = useOrigin()
+
     const [open, setOpen] = useState(false)
     const [loading, setLoading] = useState(false)
 
@@ -119,7 +122,7 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
             <Separator />
             <ApiAlert 
                 title="NEXT_PUBLIC_API_URL" 
-                description="Test desc"
+                description={`${origin}/api/${params.storeId}`}
                 variant="public"
             />
         </>
